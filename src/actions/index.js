e3fffe3d9d93713887ee80b2cb7af70eb77d3e8c
Arvin Agas <arvagas@gmail.com>
2019-10-08 23:36:51 -0400

    created login action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,19 @@
+import axios from 'axios'
+
 import axiosWithAuth from '../utils/axiosWithAuth'
 
+const POST_LOGIN_START = 'POST_LOGIN_START'
+const POST_LOGIN_SUCCESS = 'POST_LOGIN_SUCCESS'
+const POST_LOGIN_FAIL = 'POST_LOGIN_FAIL'
+
+export const login = (creds, history) => dispatch => {
+  dispatch({ type: POST_LOGIN_START })
+  axios
+    .post(`backendwebsite.com/auth/login`, creds) //will need to update with backend website
+    .then(res => {
+      dispatch({ type: POST_LOGIN_SUCCESS, payload: res.data })
+      localStorage.setItem('token', res.data.token)
+      history.push('/')
+    })
+    .catch(err => dispatch({ type: POST_LOGIN_FAIL, payload: err.response }))
+}
