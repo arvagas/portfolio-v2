bf970a74eaddf798c765bf198ac4eef570e09a44
Arvin Agas <arvagas@gmail.com>
2019-10-09 17:59:32 -0400

    created projects card with needed info

---
+++ src/components/projects/ProjectsCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ProjectsCard = () => {
+  return (
+    <div>
+      <h1>Project Name</h1>
+      <p>Description</p>
+      <p>Deploy</p>
+      <p>Github</p>
+      <p>Design</p>
+      <p>Built with:</p>
+      <p>Completed Date:</p>
+      <p>Last Updated:</p>
+    </div>
+  )
+}
+
+export default ProjectsCard
---
+++ src/components/projects/ProjectsPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import ProjectsCard from './ProjectsCard'
+
+const ProjectsPage = () => {
+  return (
+    // array.map() over each ProjectsCard
+    <ProjectsCard />
+  )
+}
+
+export default ProjectsPage
