0e7825b1cc22270c621f61c5351a671d08d4058c
Arvin Agas <arvagas@gmail.com>
2019-10-10 22:28:34 -0400

    first draft of landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      {/* Carousel hero image(s) in the background */}
+      <h1>My name is Arvin Agas.</h1>
+      <h2>Full Stack Web Developer.</h2>
+    </div>
+  )
+}
+
+export default LandingPage
