098928d6f57558174389d4cd88bea89f2291b439
Arvin Agas <arvagas@gmail.com>
2019-10-10 22:28:46 -0400

    landing page will now appear

--- src/components/router/AppRouter.js
+++ src/components/router/AppRouter.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import PrivateRoute from './PrivateRoute'
-// import LandingPage from '../LandingPage'
-// import ProjectsPage from '../projects/ProjectsPage'
+import LandingPage from '../LandingPage'
+import ProjectsPage from '../projects/ProjectsPage'
 // import LoginForm from '../forms/LoginForm'
 
 const AppRouter = () => {
   return (
     <Switch>
-      {/* <Route exact path='/' component={LandingPage} /> */}
+      <Route exact path='/' component={LandingPage} />
       <Route path='/projects' component={ProjectsPage} />
       {/* <Route path='/login' component={LoginForm} /> */}
       {/* <PrivateRoute path='/dashboard' component={} /> */}
