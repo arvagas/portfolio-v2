f08899e0cb139cf3ea95033d1c00dddc28edf335
Arvin Agas <arvagas@gmail.com>
2019-12-03 01:54:08 -0500

    style reset; global values

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+//library imports
 import React from 'react'
-
+//component imports
 import AppRouter from './components/router/AppRouter'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+//styling
+import './styles/index.scss'
 
 const App = () => {
   
