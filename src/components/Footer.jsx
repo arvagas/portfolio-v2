5b968092a2c6ed898bb534518855b2b20b1e33cb
Arvin Agas <arvagas@gmail.com>
2019-12-03 01:41:16 -0500

    foot styling (wip)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,28 +5,30 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 //image imports
 import headshot from '../assets/headshot.jpg'
+//styling
+import { StyledFooter, StyledSocialContainer, StyledProfileImage, StyledSocialLink, StyledCopyright } from '../styles/StyledComps'
 
 const Footer = () => {
   return (
-    <div>
-      <img src={headshot} alt='Picture of Arvin Agas'/>
+    <StyledFooter>
+      <StyledSocialContainer>
+        <StyledProfileImage src={headshot} alt='Picture of Arvin Agas'/>
 
-      <div>
-        <a href='https://www.github.com/arvagas'>
+        <StyledSocialLink href='https://www.github.com/arvagas'>
           <FontAwesomeIcon icon={faGithub} size="3x"/>
-        </a>
+        </StyledSocialLink>
 
-        <a href='https://www.linkedin.com/in/arvinagas/'>
+        <StyledSocialLink href='https://www.linkedin.com/in/arvinagas/'>
           <FontAwesomeIcon icon={faLinkedin} size="3x"/>
-        </a>
+        </StyledSocialLink>
 
-        <a href='mailto: arvagas@gmail.com'>
+        <StyledSocialLink href='mailto: arvagas@gmail.com'>
           <FontAwesomeIcon icon={faEnvelope} size="3x"/>
-        </a>
-      </div>
+        </StyledSocialLink>
+      </StyledSocialContainer>
 
-      <p>Copyright &copy; Arvin Agas 2019</p>
-    </div>
+      <StyledCopyright>Copyright &copy; Arvin Agas 2019</StyledCopyright>
+    </StyledFooter>
   )
 }
 
--- src/styles/StyledComps.js
+++ src/styles/StyledComps.js
@@ -1,15 +1,39 @@
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 
-//Start Navbar Styling
+//Start Navbar styling
 export const StyledNavbar = styled.div`
   display: flex;
   justify-content: flex-end;
   border-bottom: 1px solid black;
 `
-
 export const StyledNavLink = styled(NavLink)`
   padding: 1rem;
   text-decoration: none;
 `
 //End Navbar styling
+
+//Start Footer styling
+export const StyledFooter = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 1.6rem;
+  border-top: 1px solid black;
+`
+export const StyledSocialContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const StyledProfileImage = styled.img`
+  border-radius: 50%;
+  width: 100px;
+`
+export const StyledSocialLink = styled.a`
+  margin-left: 2rem;
+`
+export const StyledCopyright = styled.p`
+  text-align: center;
+`
+//End Footer styling
