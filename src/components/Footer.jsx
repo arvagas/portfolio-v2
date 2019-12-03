0788f9b839c41800ad67e171591f0503fd7e297b
Arvin Agas <arvagas@gmail.com>
2019-12-03 01:16:27 -0500

    tweaked footer html

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,31 @@
+//library imports
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+//image imports
+import headshot from '../assets/headshot.jpg'
 
 const Footer = () => {
   return (
     <div>
-      <img src={require('../assets/headshot.jpg')}/>
+      <img src={headshot} alt='Picture of Arvin Agas'/>
 
-      <a href='https://www.github.com/arvagas'>
-        <FontAwesomeIcon icon={faGithub} />
-      </a>
+      <div>
+        <a href='https://www.github.com/arvagas'>
+          <FontAwesomeIcon icon={faGithub} size="3x"/>
+        </a>
 
-      <a href='https://www.linkedin.com/in/arvinagas/'>
-        <FontAwesomeIcon icon={faLinkedin} />
-      </a>
+        <a href='https://www.linkedin.com/in/arvinagas/'>
+          <FontAwesomeIcon icon={faLinkedin} size="3x"/>
+        </a>
 
-      <a href='mailto: arvagas@gmail.com'>
-        <FontAwesomeIcon icon={faEnvelope} />
-      </a>
+        <a href='mailto: arvagas@gmail.com'>
+          <FontAwesomeIcon icon={faEnvelope} size="3x"/>
+        </a>
+      </div>
+
+      <p>Copyright &copy; Arvin Agas 2019</p>
     </div>
   )
 }
