85a066eabae6761097d76faf6368b88fc9191d13
Arvin Agas <arvagas@gmail.com>
2020-01-12 00:46:10 -0500

    removed all instances/files related to creating a backend

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
 
 import App from './App'
-import reducer from './reducers'
 
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
-const store = createStore(reducer, applyMiddleware(thunk))
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <Router>
+    <App />
+  </Router>,
 document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
