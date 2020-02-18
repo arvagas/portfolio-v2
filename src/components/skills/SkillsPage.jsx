6c493225e91186d3960b1249cf3195d6a637b31f
Arvin Agas <arvagas@gmail.com>
2020-02-18 16:04:21 -0500

    increased accessbility for skills page icons

--- src/components/skills/SkillsPage.jsx
+++ src/components/skills/SkillsPage.jsx
@@ -10,22 +10,22 @@ const SkillsPage = () => {
     <StyledSkillsPage>
       <StyledSkillsContainer>
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faHtml5} size="10x" color='#E54F26'/>
+          <FontAwesomeIcon icon={faHtml5} size="10x" color='#E54F26' title='HTML'/>
           <StyledSkillsText>HTML</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faCss3Alt} size="10x" color='#0975B9'/>
+          <FontAwesomeIcon icon={faCss3Alt} size="10x" color='#0975B9' title='CSS'/>
           <StyledSkillsText>CSS</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faJs} size="10x" color='#DAB92E'/>
+          <FontAwesomeIcon icon={faJs} size="10x" color='#DAB92E' title='JavaScript'/>
           <StyledSkillsText>JavaScript</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faReact} size="10x" color='#49C8EF'/>
+          <FontAwesomeIcon icon={faReact} size="10x" color='#49C8EF' title='React'/>
           <StyledSkillsText>React</StyledSkillsText>
         </StyledSkillsPair>
 
@@ -35,22 +35,22 @@ const SkillsPage = () => {
         </StyledSkillsPair> */}
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faNode} size="10x" color='#76B475'/>
-          <StyledSkillsText>Node.JS</StyledSkillsText>
+          <FontAwesomeIcon icon={faNode} size="10x" color='#76B475' title='Node.js'/>
+          <StyledSkillsText>Node.js</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faPython} size="10x" color='#336E9D'/>
+          <FontAwesomeIcon icon={faPython} size="10x" color='#336E9D' title='Python'/>
           <StyledSkillsText>Python</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faLess} size="10x" color='#204B7B'/>
+          <FontAwesomeIcon icon={faLess} size="10x" color='#204B7B' title='Less'/>
           <StyledSkillsText>Less</StyledSkillsText>
         </StyledSkillsPair>
 
         <StyledSkillsPair>
-          <FontAwesomeIcon icon={faSass} size="10x" color='#CD679A'/>
+          <FontAwesomeIcon icon={faSass} size="10x" color='#CD679A' title='Sass'/>
           <StyledSkillsText>Sass</StyledSkillsText>
         </StyledSkillsPair>
       </StyledSkillsContainer>
