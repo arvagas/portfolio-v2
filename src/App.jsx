c7c1d70fd49b5bba7f3ad588e51b4766d40b2219
Arvin Agas <arvagas@gmail.com>
2020-02-19 18:45:15 -0500

    bg images will no longer rotate, instead one will be randomly chosen

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,38 @@ import bg3 from './assets/backgrounds/bg3.jpg'
 import { StyledApp } from './styles/StyledComps'
 import './styles/index.scss'
 
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
 const App = () => {
   const [bgImg, setBgImg] = useState()
-  const [arrIndex, setArrIndex] = useState(0)
+  // const [arrIndex, setArrIndex] = useState(0)
   // shuffle this array eventually
   const imgArr = [bg1, bg2, bg3]
 
   // load initial bg
   useEffect(() => {
     if (!bgImg) {
-      setBgImg(imgArr[arrIndex])
-      setArrIndex(arrIndex => arrIndex + 1)
+      let randomIndex = getRandomInt(imgArr.length)
+      setBgImg(imgArr[randomIndex])
+      // setArrIndex(arrIndex => arrIndex + 1)
     }
   }, [])
 
   // change background every 45 seconds
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (arrIndex < imgArr.length) {
-        setBgImg(imgArr[arrIndex])
-        setArrIndex(arrIndex => arrIndex + 1)
-      } else {
-        setBgImg(imgArr[0])
-        setArrIndex(1)
-      }
-    }, 45000)
-    return () => clearInterval(interval)
-  })
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (arrIndex < imgArr.length) {
+  //       setBgImg(imgArr[arrIndex])
+  //       setArrIndex(arrIndex => arrIndex + 1)
+  //     } else {
+  //       setBgImg(imgArr[0])
+  //       setArrIndex(1)
+  //     }
+  //   }, 45000)
+  //   return () => clearInterval(interval)
+  // })
   
   return (
     <StyledApp style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(${bgImg})` }}>
