d11c082ace785aa2dead08de57b6c72c2f76616b
Arvin Agas <arvagas@gmail.com>
2020-02-21 14:45:51 -0500

    about page foundation complete; tweaked contact submit button and navlink active

--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 // component imports
 import LandingPage from '../LandingPage'
 import ProjectsPage from '../projects/ProjectsPage'
+import AboutPage from '../about/AboutPage'
 import SkillsPage from '../skills/SkillsPage'
 import ContactPage from '../contact/ContactPage'
 
@@ -12,7 +13,7 @@ const AppRouter = () => {
     <Switch style={{flex: 1}}>
       <Route exact path='/' component={LandingPage} />
       <Route path='/projects' component={ProjectsPage} />
-      {/* <Route path='/about' component={} /> */}
+      <Route path='/about' component={AboutPage} />
       <Route path='/skills' component={SkillsPage} />
       <Route path='/contact' component={ContactPage} />
     </Switch>
