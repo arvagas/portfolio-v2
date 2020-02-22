c4b4544f1953283b5660d234abc6520fd136137c
Arvin Agas <arvagas@gmail.com>
2020-02-21 23:57:59 -0500

    about page content pretty good!

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -13,9 +13,9 @@ const AboutPage = () => {
 
         <StyledAboutContent>
           <StyledAboutHeader>About Me</StyledAboutHeader>
-          <StyledAboutPara>This section will be about me</StyledAboutPara>
-          <StyledAboutPara>This section will be about me</StyledAboutPara>
-          <StyledAboutPara>This section will be about me</StyledAboutPara>
+          <StyledAboutPara>I am a full stack web developer. I specialize in developing single-page applications utilizing React, as well as creating back ends with frameworks such as Express and Django.</StyledAboutPara>
+          <StyledAboutPara>My experience in the food and health care industry over the years has strengthened my interpersonal skills and taught me the importance of patience and compassion for the customer or patient. Applying these assets to web development can and will yield high quality results that will benefit both company and consumer.</StyledAboutPara>
+          <StyledAboutPara>When I'm not coding, you can catch me watching gratuitous amounts of television, doing some experimental cooking, bicycling all over the place, and play some rage-inducing tabletop games with my enemies. Okay, fine... my friends.</StyledAboutPara>
         </StyledAboutContent>
       </StyledAboutContainer>
     </StyledAboutPage>
