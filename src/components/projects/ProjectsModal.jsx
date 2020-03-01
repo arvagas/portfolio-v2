39cf76da791913bc571d5450695223abc899529c
Arvin Agas <arvagas@gmail.com>
2020-03-01 11:09:27 -0500

    alt tag also dependent on available gif

--- src/components/projects/ProjectsModal.jsx
+++ src/components/projects/ProjectsModal.jsx
@@ -20,7 +20,7 @@ const ProjectsModal = ({ project, showModal, handleModal }) => {
     <StyledProjectsModal>
       <StyledModalTitle>{project.title}</StyledModalTitle>
       <StyledModalX onClick={event => handleModal(event)}>X</StyledModalX>
-      <StyledModalPhoto src={project.gif ? project.gif : project.photo} alt={project.gifAlt ? project.gifAlt : project.gifPhoto}/>
+      <StyledModalPhoto src={project.gif ? project.gif : project.photo} alt={project.gif ? project.gifAlt : project.gifPhoto}/>
 
       <StyledModalMiddle>
         <div>
